437127731cbf1f4cb60b6d0eb31fbb4600d83621
Stijndus <99053054@mydavinci.nl>
2020-09-08 12:23:05 +0200

    Ik heb Lab 1 Lab 2 en de eerste opdracht van lab 3 toegevoegd

---
+++ b1w2l2.js
@@ -0,0 +1,9 @@
+// begin hier je JavaScript commandos
+// Stijn Dusseldorp
+// Software Developer
+
+alert("Je moet eventjes 3 kleine vraagjes beantwoorden"); // Dit geeft de alert die zegt dat je wat vragen moet beantwoorden
+var firstName = prompt("Wat is je voornaam?"); // Dit is de prompt die firstName een waarde geeft
+var lastName = prompt("Wat is je achternaam?"); // Dit is de prompt die lastName een waarde geeft
+var age = prompt("Wat is je leeftijd?"); // Dit is de prompt die age een waarde geeft
+document.write("Voornaam: " + firstName + "<br>Achternaam: " + lastName + "<br>Leeftijd: " + age); // Dit laat op de pagina de waarde van de 3 variable zien
